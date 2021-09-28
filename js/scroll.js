8a8ec9cd89f2c8139702f132cad53da7095ec188
Qabas al ani <doctorqa@yahoo.com>
2021-09-28 17:44:32 -0400

    updating my portfolio and added my second page to it with contact information

---
+++ js/scroll.js
@@ -0,0 +1,54 @@
+const sr = ScrollReveal({
+    distance: '600px',
+    duration: 2500,
+  
+  });
+  
+  sr.reveal(`.landing-title`, {
+    delay: 600, origin: 'left'
+  });
+  sr.reveal(`.landing-description`, {
+    delay: 600, origin: 'right'
+  });
+
+  sr.reveal(`.language-1`, {
+    delay: 100, origin: 'right'
+  });
+
+  sr.reveal(`.language-2`, {
+    delay: 200, origin: 'right'
+  });
+
+  sr.reveal(`.language-3`, {
+    delay: 300, origin: 'right'
+  });
+
+  sr.reveal(`.language-4`, {
+    delay: 400, origin: 'right'
+  });
+
+  sr.reveal(`.language-5`, {
+    delay: 500, origin: 'right'
+  });
+
+  sr.reveal(`.language-6`, {
+    delay: 600, origin: 'right'
+  });
+
+  sr.reveal(`.language-7`, {
+    delay: 700, origin: 'right'
+  });
+
+  sr.reveal(`.language-8`, {
+    delay: 800, origin: 'right'
+  });
+
+  sr.reveal(`.language-9`, {
+    delay: 900, origin: 'right'
+  });
+
+
+
+
+
+
