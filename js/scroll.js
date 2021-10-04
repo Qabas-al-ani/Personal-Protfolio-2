e5b3ea3e66728cc5d6baaa745ec03d69ef5ad11d
Qabas al ani <doctorqa@yahoo.com>
2021-10-03 21:00:06 -0400

    fixed the mobile version and added slise navabar for it

--- js/scroll.js
+++ js/scroll.js
@@ -1,54 +1,63 @@
 const sr = ScrollReveal({
-    distance: '600px',
-    duration: 2500,
-  
-  });
-  
-  sr.reveal(`.landing-title`, {
-    delay: 600, origin: 'left'
-  });
-  sr.reveal(`.landing-description`, {
-    delay: 600, origin: 'right'
-  });
+  distance: "100px",
+  duration: 1000,
+  mobile: true,
+});
 
-  sr.reveal(`.language-1`, {
-    delay: 100, origin: 'right'
-  });
+sr.reveal(`.landing-title`, {
+  delay: 200,
+  origin: "left",
+});
+sr.reveal(`.landing-description`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-2`, {
-    delay: 200, origin: 'right'
-  });
+sr.reveal(`.language-1`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-3`, {
-    delay: 300, origin: 'right'
-  });
+sr.reveal(`.language-2`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-4`, {
-    delay: 400, origin: 'right'
-  });
+sr.reveal(`.language-3`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-5`, {
-    delay: 500, origin: 'right'
-  });
+sr.reveal(`.language-4`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-6`, {
-    delay: 600, origin: 'right'
-  });
+sr.reveal(`.language-5`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-7`, {
-    delay: 700, origin: 'right'
-  });
+sr.reveal(`.language-6`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-8`, {
-    delay: 800, origin: 'right'
-  });
+sr.reveal(`.language-7`, {
+  delay: 200,
+  origin: "right",
+});
 
-  sr.reveal(`.language-9`, {
-    delay: 900, origin: 'right'
-  });
+sr.reveal(`.language-8`, {
+  delay: 200,
+  origin: "right",
+});
 
+sr.reveal(`.language-9`, {
+  delay: 200,
+  origin: "right",
+});
 
-
-
-
-
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
